40346982ae32872acdb9952dccc4df7f64805ce6
Валентина <ikherty@gmail.com>
2020-02-04 11:55:21 +0300

    1st lab

--- firstpro/src/firstpro/Firstpro.java
+++ firstpro/src/firstpro/Firstpro.java
@@ -6,17 +6,21 @@
 package firstpro;
 
 /**
- *
+ *Описание класса
  * @author petrenko
  */
 public class Firstpro {
 
     /**
+     * Описание метода
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");//sout+tab
+        //Sy+ctrl+space
+        NewJFrame frame=new NewJFrame();
+        frame.setVisible(true);
     }
     
 }
